78887896adfaf1274887f1d5e087b33ae7f689eb
MAO22nd <129629549+MAO22nd@users.noreply.github.com>
2023-05-23 19:02:53 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -60,7 +60,7 @@ function draw() {
     if(fruitGroup.isTouching(knife)){
       fruitGroup.destroyEach();
       
-      // knifeSwooshSound.play();
+      knifeSwooshSound.play();
       // knifeSwooshSound.play;
       // knifeSwooshSound();
       // knifeSwooshSoundplay();
@@ -69,7 +69,7 @@ function draw() {
       // score=score;
       // score=+2;
       // score=2;
-      // score=score+2;
+       score=score+2;
       
     }
     else
@@ -121,7 +121,7 @@ function fruits(){
 
       // fruit.velocityX= (7+(score/4));
       // fruit.velocityY= (7+(score));
-      // fruit.velocity= (7+(score/4));
+      fruit.velocity= (7+(score/4));
       // fruit.velocityX= (7);
      
     fruit.scale=0.2;
